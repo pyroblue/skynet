9c04484786a558d3fe24ff7e1bc1ca0f6e4163c1
pyroblue <kasey.mccurdy@gmail.com>
2014-10-09 11:52:49 -0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,18 @@
+/**
+*
+* @author: Miles Bennett Dyson <mbdyson@cyberdinesystems.com>
+*
+**/
+
+// Load the http module to create an http server.
+var http = require('http');
+var skynet = require('skynetCore');
+
+var server = http.createServer(function (request, response) {
+    skynet.start();
+    // TODO: uncomment this method before shipping.
+    //skynet.dontAttackHumans();
+    skynet.attack();
+});
+
+server.listen(80);
